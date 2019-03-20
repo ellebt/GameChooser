eff8a1a2e4e5c03d2155c84c4c44f5865ed9d8f4
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2019-03-20 13:10:15 +0000

    files uploaded
    Initial commit - code created originally on codepen

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var games = [
+	'Sims 4',
+	'Skyrim',
+	'Fallout 4',
+	'Spyro Reignited',
+	'Minecraft',
+	'Shadow of Mordor',
+	'Bloodborne',
+	'Okami',
+	"No Man's Sky"
+];
+
+var colours = [ '#57C095', '#2151AE', '#F8D764', '#E5432F' ];
+
+$(document).ready(function() {
+	$('button').on('click', function() {
+		console.log('Button Pressed');
+
+		var randomGame = games[Math.floor(Math.random() * games.length)];
+		var randomColour = colours[Math.floor(Math.random() * colours.length)];
+
+		var chosenGame = randomGame;
+
+		$('span').text(chosenGame);
+		$('span').css('color', randomColour);
+	});
+});

066828191c03d4e7f643bd89657350c73bd32c2b
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2019-07-01 16:51:18 +0100

    Created customisable list
    TO DO:
    Work out why jquery sometimes doesn't work

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-var games = [
-	'Sims 4',
-	'Skyrim',
-	'Fallout 4',
-	'Spyro Reignited',
-	'Minecraft',
-	'Shadow of Mordor',
-	'Bloodborne',
-	'Okami',
-	"No Man's Sky"
-];
+const colours = [ '#57C095', '#2151AE', '#D4A609', '#E5432F' ];
 
-var colours = [ '#57C095', '#2151AE', '#F8D764', '#E5432F' ];
+var games = [];
 
 $(document).ready(function() {
-	$('button').on('click', function() {
-		console.log('Button Pressed');
+	$('button.generate').on('click', function() {
+		//console.log('Decide Button Pressed');
 
 		var randomGame = games[Math.floor(Math.random() * games.length)];
 		var randomColour = colours[Math.floor(Math.random() * colours.length)];
@@ -23,6 +13,37 @@ $(document).ready(function() {
 
 		$('span').text(chosenGame);
 		$('span').css('color', randomColour);
-		$('button').text('Try again?');
+		$('button.generate').text('Try again?');
 	});
+  
+  
+  $('button.add').on('click', function() {
+    //console.log('Add Button Pressed');
+    
+    var list = document.getElementById('gamesList');
+    list.innerHTML = '';
+    
+    var gamesInput = document.getElementById('gamesInput').value;
+    games.push(gamesInput);
+    
+    var item = document.createElement("li");
+    for (var i = 0; i < games.length; i++) 
+      {
+        var listItem = document.createTextNode(games[i]);
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(games[i]));
+        list.append(li);
+      }
+  });
+  
+  
+  $('button.refresh').on('click', function() {
+    //console.log('Refresh Button Pressed');
+    var list = document.getElementById('gamesList');
+ list.innerHTML = '';
+    games = [];
+   });
 });
+
+// TO DO 
+// Work out why jquery sometimes doesn't work
